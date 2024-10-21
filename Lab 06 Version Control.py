b41383a51bbec6651707c4801634c2a77ab84bd3
Kimgun3383 <tygob5247@gmail.com>
2024-10-21 22:04:12 +0900

    Add files via upload

---
+++ Lab 06 Version Control.py
@@ -0,0 +1,45 @@
+def encode(data):
+    string = ""
+    for item in data:
+        item = int(item)
+        item +=  3
+        item = str(item)
+        if item == '10':
+            item = str(0)
+        if item == '11':
+            item = str(1)
+        if item == '12':
+            item = str(2)
+        string += item
+    return string
+
+def decode(data):
+    string = ""
+    for item in data:
+        item = int(item)
+        item -= 3
+        if item == -1:
+            item = 9
+        elif item == -2:
+            item = 8
+        elif item == -3:
+            item = 7
+        string += str(item)
+    return string
+
+if __name__ == '__main__':
+    player_continue = True
+    while player_continue:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+        player_choice = float(input("Please enter an option: "))
+        if player_choice == 1:
+            password = str(input("Please enter your password to encode: "))
+            password = encode(password)
+            print("Your password has been encoded and stored!")
+            print()
+        elif player_choice == 2:
+            print(f"The encoded password is {password}, and the original password is {decode(password)}")
+        elif player_choice == 3:
+            exit()
+        elif player_choice != (1, 2, 3):
+            print("Invalid input!\nPlease enter an integer value between 1 and 3.\n")
